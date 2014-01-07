14973b831bd124f527dec1c1d8888e0c50c5314b
Jennifer <Jennifer@G62Jenny>
2014-01-07 11:55:05 +0100

    Creates

--- ItProjekt/src/de/hdm/itprojekt/client/gui/CreateStundenplaneintrag.java
+++ ItProjekt/src/de/hdm/itprojekt/client/gui/CreateStundenplaneintrag.java
@@ -1,26 +1,19 @@
 package de.hdm.itprojekt.client.gui;
 
-import java.util.ArrayList;
 import java.util.Vector;
 
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
-import de.hdm.itprojekt.shared.Verwaltungsklasse;
-import de.hdm.itprojekt.shared.VerwaltungsklasseAsync;
-import de.hdm.itprojekt.shared.bo.*;
-import de.hdm.itprojekt.client.ItProjekt;
+import de.hdm.itprojekt.shared.*;
+import de.hdm.itprojekt.shared.bo.Stundenplaneintrag;
 
 
 	/**
